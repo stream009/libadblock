a413b5efb39c21bdaf5cb3cdc5d8aadb21f6ff8c
stream <stream009@gmail.com>
2019-05-02 12:41:27 +0900

    bug fix

--- benchmark/memory_usage.cpp
+++ benchmark/memory_usage.cpp
@@ -1,4 +1,4 @@
-#include <adblock/api.h>
+#include <adblock/database.hpp>
 
 #include <cassert>
 #include <cstring>
@@ -6,42 +6,6 @@
 #include <iostream>
 
 namespace fs = std::filesystem;
-
-class adblock_db
-{
-public:
-    adblock_db()
-    {
-        m_handle = ::adblock_create();
-        assert(m_handle);
-    }
-
-    ~adblock_db()
-    {
-        ::adblock_destroy(m_handle);
-    }
-
-    void add_filter_list(fs::path const& data_path)
-    {
-        ::adblock_add_filter_set(
-            m_handle,
-            data_path.c_str(), std::strlen(data_path.c_str())
-        );
-    }
-
-    void print_statistics() const
-    {
-        char const* json = nullptr;
-        size_t json_len = 0;
-
-        ::adblock_statistics(m_handle, &json, &json_len);
-
-        std::cout << json << "\n";
-    }
-
-private:
-    ::adblock_t m_handle;
-};
 
 int
 main()
@@ -54,7 +18,7 @@ main()
         return -1;
     }
 
-    adblock_db adb;
+    adblock::database adb;
     adb.add_filter_list(data_path);
 
     //adb.print_statistics();
